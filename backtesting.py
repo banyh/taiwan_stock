57dfc6d9c72e41fbcee5686f92b79fd5d2f23245
banyh <banyhong@gliacloud.com>
2018-04-11 11:19:11 +0800

    add buy/sell fee

--- backtesting.py
+++ backtesting.py
@@ -7,6 +7,14 @@ from dataset import dailydata
 one_day = timedelta(days=1)
 
 
+def buy_fee(amount):
+    return amount * 0.001425
+
+
+def sell_fee(amount):
+    return amount * 0.004425
+
+
 def backtest(broker, start_money, start_day=date(2017, 1, 1), end_day=date(2017, 12, 31)):
     broker.money = float(start_money)
     broker.portfolio = defaultdict(int)
@@ -18,13 +26,13 @@ def backtest(broker, start_money, start_day=date(2017, 1, 1), end_day=date(2017,
         actions = broker.append(data)
         for act, stock_id, price, num_shares in actions:
             if act == 'buy':
-                broker.money -= price * num_shares
+                broker.money -= price * num_shares + buy_fee(price * num_shares)
                 broker.portfolio[stock_id] += num_shares
                 assert broker.money >= 0
                 print('{!s}，以{}的價格買進{}股{}，餘額{}'.format(today, price, num_shares, stock_id, broker.money))
             elif act == 'sell':
                 broker.portfolio[stock_id] -= num_shares
-                broker.money += price * num_shares
+                broker.money += price * num_shares - sell_fee(price * num_shares)
                 assert broker.portfolio[stock_id] >= 0
                 print('{!s}，以{}的價格賣出{}股{}，餘額{}'.format(today, price, num_shares, stock_id, broker.money))
         today += one_day
@@ -34,7 +42,7 @@ def backtest(broker, start_money, start_day=date(2017, 1, 1), end_day=date(2017,
         if broker.portfolio[stock_id] > 0:
             price = last_trading_data[stock_id + '_收盤價']
             num_shares = broker.portfolio[stock_id]
-            broker.money += price * num_shares
+            broker.money += price * num_shares - sell_fee(price * num_shares)
             print('以{}的價格賣出{}股{}，餘額{}'.format(price, num_shares, stock_id, broker.money))
 
     print('啟始資金={}，投資報酬率={:.2f}%'.format(start_money, broker.money / start_money * 100 - 100))
